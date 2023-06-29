c0c74aee894928f4353cdc9b37aa89318f7d3ca7
Udassar <udginsmail@gmail.com>
2023-06-29 16:24:54 +0300

    Решил 3ю задачу

--- Program.cs
+++ Program.cs
@@ -30,24 +30,37 @@
 // 82 -> 10
 // 9012 -> 12
 
-Console.Write("Введите число : ");
-int num = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите число : ");
+// int num = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine($"{GetSum(num)}");
+// Console.WriteLine($"{GetSum(num)}");
 
-int GetSum(int number)
-{
-  int sum = 0;
-  while (number > 0)
-  {
-    sum = sum + number % 10;
-    number /= 10;
-  }
-  return sum;
-}
+// int GetSum(int number)
+// {
+//   int sum = 0;
+//   while (number > 0)
+//   {
+//     sum = sum + number % 10;
+//     number /= 10;
+//   }
+//   return sum;
+// }
 
 
 
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. (можно указать любой промежуток)
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
+
+
+int[] array = GetArray(8,0,100);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+int[] GetArray(int size, int minValue, int maxValue){
+  int[] result = new int[size];
+  for(int i = 0; i < size; i++){
+    result[i] = new Random().Next(minValue, maxValue);
+  }
+  return result;
+}
